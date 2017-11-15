1a99ee3a6a9a0e09a4dc42f78e232e2dfdf21596
shewes <sh62@rice.edu>
2017-11-14 18:35:22 -0600

    added laser function and additional pouring animation

--- waste_sensor_v1/waste_sensor_v1.ino
+++ waste_sensor_v1/waste_sensor_v1.ino
@@ -1,24 +1,36 @@
+/* Waste sensor
+ *  Monitors the liquid level in a tissue culture waste flask and responds
+ *  to the flask being filled past a threshold and emptied. 
+ *  
+ *  Laser attaches to GND (black) and pin #9 (red)
+ *  
+ *  by Alyssa Shapiro and Sarah Hewes
+ */
+ 
 #include <Adafruit_CircuitPlayground.h>
 
+// initialize values
 int value;
-int escalation;
-boolean flaskfull;
-float X, Y, Z;
+int escalation = 0;
+boolean flaskfull = false;
+float X;
 int count;
 
 void setup() {
-  // put your setup code here, to run once:
+  //start the serial output
   Serial.begin(9600);
+  //start the circuit plaground
   CircuitPlayground.begin();
-  escalation = 0;
-  flaskfull = false;
+  //change the mode of pin #9 to output
+  pinMode(9, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   
   //turn laser on and check light level
+  digitalWrite(9, HIGH);
   value = CircuitPlayground.lightSensor();
+  digitalWrite(9,LOW);
   
   Serial.print("Light Sensor: ");
   Serial.println(value);
@@ -29,8 +41,10 @@ void loop() {
     //wait 15 min and test again
     delay(1000); //15 min power saving mode
     
-    //turn laser on and check light level
-    value = CircuitPlayground.lightSensor();
+      //turn laser on and check light level
+      digitalWrite(9, HIGH);
+      value = CircuitPlayground.lightSensor();
+      digitalWrite(9,LOW);
     
     if (value < 100) {    
             //beep a lot
@@ -62,19 +76,20 @@ void loop() {
                     if (X > 1) {
                       Serial.print("5 second check");
                       count = 0;
-                      for (int j=0; j<=5; j++){
+                      for (int j=0; j<=10; j++){
                         X = CircuitPlayground.motionX();
                         if (X > 1){
                           count++;
+                          CircuitPlayground.setPixelColor(j, 0,   255,   0);
                           }
-                        delay(1000);                        
+                        delay(500);                        
                       }
-                      if (count >= 5){
+                      if (count >= 10){
                         escalation = 0;
                         flaskfull = false;
-                        //green light
+                        //blue light
                          for (int x=0; x <= 9; x++) {
-                       CircuitPlayground.setPixelColor(x, 0,   255,   0);
+                       CircuitPlayground.setPixelColor(x, 0,   0,   255);
                            
                        }     
                        //while x is positive (flask inverted) wait
