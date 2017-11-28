0cca8e0e8ebfc5e6b16711ec14f5ef332ee32627
shewes <33137245+shewes@users.noreply.github.com>
2017-11-28 14:19:45 -0600

    added comments

--- myemail.py
+++ myemail.py
@@ -1,18 +1,25 @@
-﻿import serial
+#Cell culture waste sensor (additional email functionality)
+#Alyssa Shapiro and Sarah Hewes 2017
+#When run approximately every 15 minutes on a chrontab, this code will allow the raspberry pi to obtain serial information from
+#the circuit playground and send emails to the student and PI involved with cell culture maintance
+
+import serial
 import time
 import smtplib
 
 #open the serial port
 ser = serial.Serial('/dev/ttyACM0', 9600)
 
-time.sleep(5)
+#pause while the serial feed fills
+time.sleep(10)
 
+#read a line from the serial feed
 read_serial = ser.readline()
 
+#initialize email information
 smtpUser = 'alyssamshapiro@gmail.com'
-smtpPass = 'dancechica'
+smtpPass = '********' #password redacted
 gradstudent = 'sh62@rice.edu'
-
 fromAdd = smtpUser
 
 s = smtplib.SMTP('smtp.gmail.com',587)
@@ -22,7 +29,8 @@ s.ehlo()
 
 s.login(smtpUser,smtpPass)
 
-
+#if the serial feed shows various keywords then send emails of increasing urgency
+#first two emails go to the student. the last one goes to the PI
 if read_serial.find("kitten") > -1: 
 	subject = 'Its your turn to change the waste container!'
 	header = 'To: ' + gradstudent + '\n' + 'From: ' + fromAdd + '\n' + 'Subject: ' + subject
